921a501c13fdb332a86f74384e75e2c919c6c2b3
Khushal <khushalnariya912@gmail.com>
2026-05-29 22:22:04 +0530

    Enable CORS_ALLOW_ALL_ORIGINS in settings.py

--- e_waste_recycling/settings.py
+++ e_waste_recycling/settings.py
@@ -164,10 +164,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:2000",
-    "http://127.0.0.1:2000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 # CSRF_TRUSTED_ORIGINS = [
 #     "http://localhost:2000",
 # ]
