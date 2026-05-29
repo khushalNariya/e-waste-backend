04103e6c39c827e5ba7444630b1751da654c3d05
Khushal <khushalnariya912@gmail.com>
2026-05-29 20:17:33 +0530

    live fix

--- e_waste_recycling/settings.py
+++ e_waste_recycling/settings.py
@@ -162,8 +162,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:2000",
+    "http://127.0.0.1:2000",
+]
 # CSRF_TRUSTED_ORIGINS = [
 #     "http://localhost:2000",
 # ]
